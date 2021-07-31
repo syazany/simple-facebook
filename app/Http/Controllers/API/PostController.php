b5fe5c2ccf53848e432abfdc72067c2b248ead24
Muhammad Syazany <shuhaimi.syazany@gmail.com>
2021-07-31 21:59:51 +0800

    working on comments

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\PostCreateRequest;
 use App\Http\Resources\Post\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -14,23 +15,23 @@ class PostController extends Controller
         return PostResource::collection($request->user()->posts()->paginate());
     }
 
-    public function store(Request $request)
+    public function store(PostCreateRequest $request)
     {
-        //
+        return new PostResource($request->user()->posts()->create([$request->validated()]));
     }
 
    public function show(Post $post)
     {
-        //
+        return new PostResource($post);
     }
 
-    public function update(Request $request, Post $post)
+    public function update(PostCreateRequest $request, Post $post)
     {
-        //
+        return new PostResource(tap($post)->update($request->validated()));
     }
 
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
