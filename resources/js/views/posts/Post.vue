3eae1f9ec8018aba105b91d434e55c73a9d64875
muhdsyazany97 <syazany1997@gmail.com>
2021-08-01 23:48:05 +0800

    change like button color to blue

--- resources/js/views/posts/Post.vue
+++ resources/js/views/posts/Post.vue
@@ -17,7 +17,8 @@
                    class="text-blue-500 hover:underline">Add comments ({{post.comments.length}} comments)</a>
                 <button
                  @click="likePost"
-                 class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                 class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white
+                 py-2 px-4 border border-blue-500 hover:border-transparent rounded inline-flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
                          viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
